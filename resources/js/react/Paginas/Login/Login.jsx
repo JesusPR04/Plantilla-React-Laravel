99adbb92c3ac45a50b902f7e728647c8f20b61c2
JosePalmax <jopalmaca@gmail.com>
2024-04-24 10:55:57 +0200

    Arreglo login

--- resources/js/react/Paginas/Login/Login.jsx
+++ resources/js/react/Paginas/Login/Login.jsx
@@ -29,70 +29,6 @@ const Login = () => {
     };
 
     return (
-<<<<<<< HEAD
-        <section className="bg-gray-50">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow border mt-14 mb-14 sm:max-w-md xl:p-0">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-colorFuente md:text-2xl">
-                            Iniciar sesión
-                        </h1>
-                        <div className="space-y-4 md:space-y-6">
-                            <div>
-                                <label
-                                    for="email"
-                                    className="block mb-2 text-sm font-medium text-colorFuente"
-                                >
-                                    Tu correo electrónico
-                                </label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className="bg-gray-50 border border-gray-300 text-colorFuente sm:text-sm rounded-lg
-                                     focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    placeholder="name@company.com"
-                                    onChange={(e) => cambiarEmail(e)}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    for="password"
-                                    className="block mb-2 text-sm font-medium text-colorFuente"
-                                >
-                                    Contraseña
-                                </label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    placeholder="••••••••"
-                                    className="bg-gray-50 border border-gray-300 text-colorFuente sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500
-                                     block w-full p-2.5"
-                                    onChange={(e) => cambiarPassword(e)}
-                                    required
-                                />
-                            </div>
-                            <button
-                                type="submit"
-                                className="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none
-                                 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={() => iniciarSesion()}
-                            >
-                                Iniciar sesión
-                            </button>
-                            <p className="text-sm font-light text-colorFuente">
-                                ¿ Todavía no tienes una cuenta ?{" "}
-                                <Link
-                                    to="/register"
-                                    className="font-medium text-blue-500 hover:underline"
-                                >
-                                    Regístrate
-                                </Link>
-                            </p>
-                        </div>
-                    </div>
-=======
         <section className="bg-gray-50 flex flex-wrap justify-center ">
             <div className="relative mt-14 mb-14 max-w-[500px] hidden lg:block lg:w-1/2 overflow-hidden">
                 <img
@@ -137,7 +73,7 @@ const Login = () => {
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-colorFuente md:text-2xl">
                         Inicia sesión en su cuenta
                     </h1>
-                    <form className="space-y-4 md:space-y-6">
+                    <div className="space-y-4 md:space-y-6">
                         <div>
                             <label
                                 htmlFor="email"
@@ -152,6 +88,7 @@ const Login = () => {
                                 className="bg-gray-50 border border-gray-300 text-colorFuente sm:text-sm rounded-lg
                                   focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                 placeholder="name@company.com"
+                                onChange={(e) => cambiarEmail(e)}
                                 required
                             />
                         </div>
@@ -169,6 +106,7 @@ const Login = () => {
                                 placeholder="••••••••"
                                 className="bg-gray-50 border border-gray-300 text-colorFuente sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500
                                   block w-full p-2.5"
+                                  onChange={(e) => cambiarPassword(e)}
                                 required
                             />
                         </div>
@@ -176,6 +114,7 @@ const Login = () => {
                             type="submit"
                             className="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none
                               focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                              onClick={() => iniciarSesion()}
                         >
                             Iniciar sesión
                         </button>
@@ -188,8 +127,7 @@ const Login = () => {
                                 Regístrate
                             </Link>
                         </p>
-                    </form>
->>>>>>> 51e8a114a64eb9ee97128da8aed3a2c82916aec0
+                    </div>
                 </div>
             </div>
         </section>
