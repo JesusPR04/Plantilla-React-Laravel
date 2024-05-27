9a66b2a7b0994ceed20d682303eb476a9e5f221d
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-05-27 18:51:50 +0200

    Desplegable

--- resources/js/react/Components/Header.jsx
+++ resources/js/react/Components/Header.jsx
@@ -181,13 +181,12 @@ function Header() {
                                     <button
                                         id="dropdownNavbarLink"
                                         data-dropdown-toggle="dropdownNavbar"
-                                        className="py-2 px-3 text-colorFuente rounded-2xl 
-                                        hover:bg-[#f8f7fa] hover:text-blue-500 flex flex-row
-                                        items-center"
+                                        className="py-2 px-3 text-white rounded-md 
+                                       bg-blue-500 flex flex-row items-center"
                                     >
                                         <span>{user.nombre}</span>
                                         <svg
-                                            className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            className="w-2.5 h-2.5 ms-2.5 hover:w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                                         </svg>
                                     </button>
