01af7fae40b9c8756a3245b22c6b6e0553b47797
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-05-28 23:02:27 +0200

    Maquetacion editar perfil falta la ciudad

--- resources/js/react/Paginas/Perfil/Editar.jsx
+++ resources/js/react/Paginas/Perfil/Editar.jsx
@@ -18,12 +18,14 @@ const Editar = () => {
     });
 
     const [ciudades, setCiudades] = useState([]);
+    const [userCity, setUserCity] = useState('')
 
     useEffect(() => {
         const getUserData = async () => {
             try {
                 const data = await fetchUserData(); // Implementa la función fetchUserData para obtener los datos del usuario
                 setUserData(data);
+                setUserCity(ciudad => ciudad = data.ciudad); // Recoger la ciudad del usuario
             } catch (error) {
                 console.error('Error fetching user data:', error);
             }
@@ -42,7 +44,7 @@ const Editar = () => {
     const cambiarCiudad = (e) => {
         setUserData({
             ...userData,
-            ciudad: e,
+            ciudad: e.label,
         });
     };
 
@@ -64,54 +66,67 @@ const Editar = () => {
     }, []);
 
     if (!userData) {
-        return <div className="min-h-[calc(100vh-436px)] flex items-center justify-center">Loading...</div>;
+        return <div className="min-h-[calc(100vh-436px)] flex items-center justify-center">Cargando...</div>;
     }
 
 
     return (
-        <div className="min-h-[calc(100vh-436px)] bg-gray-100 flex flex-col items-center p-6">
-            <div className="w-full max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+        <div className="min-h-[calc(100vh-436px)] bg-gray-100 flex flex-col items-center p-6 text-colorFuente">
+            <div className="w-full max-w-xl bg-white shadow-lg rounded-lg overflow-hidden">
                 <div className="p-6">
-                    <h2 className="text-2xl font-bold text-center mb-4">Editar Perfil</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-4">
-                            <label htmlFor="nombre" className="block text-sm font-medium text-gray-700">Nombre</label>
+                    <h2 className="text-2xl font-bold text-center mb-4 uppercase">Editar <span className='text-blue-500'>Perfil</span></h2>
+                    <form className='mx-auto space-y-4 md:space-y-6 grid grid-cols-2 gap-x-8 items-end' 
+                        onSubmit={handleSubmit}
+                    >
+                        <div className="col-span-2 sm:col-span-1">
+                            <label htmlFor="nombre" className="block text-sm font-medium text-colorFuente">Nombre</label>
                             <input
                                 type="text"
                                 id="nombre"
                                 name="nombre"
                                 value={userData.nombre}
                                 onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="mt-1 block w-full px-3 py-2 border bg-gray-50 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 required
                             />
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="apellidos" className="block text-sm font-medium text-gray-700">Apellidos</label>
+                        <div className="col-span-2 sm:col-span-1">
+                            <label htmlFor="apellidos" className="block text-sm font-medium text-colorFuente">Apellidos</label>
                             <input
                                 type="text"
                                 id="apellidos"
                                 name="apellidos"
                                 value={userData.apellidos}
                                 onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="mt-1 block w-full px-3 py-2 border bg-gray-50 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 required
                             />
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Correo Electrónico</label>
+                        <div className="col-span-2 sm:col-span-1">
+                            <label htmlFor="email" className="block text-sm font-medium text-colorFuente">Correo Electrónico</label>
                             <input
                                 type="email"
                                 id="email"
                                 name="email"
                                 value={userData.email}
                                 onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="mt-1 block w-full px-3 py-2 border bg-gray-50 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 required
                             />
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="ciudad" className="block text-sm font-medium text-gray-700">Ciudad</label>
+                        <div className="col-span-2 sm:col-span-1">
+                            <label htmlFor="telefono" className="block text-sm font-medium text-colorFuente">Teléfono</label>
+                            <input
+                                type="text"
+                                id="telefono"
+                                name="telefono"
+                                value={userData.telefono}
+                                onChange={handleChange}
+                                className="mt-1 block w-full px-3 py-2 border bg-gray-50 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <label htmlFor="ciudad" className="block text-sm font-medium text-colorFuente">Ciudad</label>
                             <Select
                                 options={ciudades.map((ciudad) => ({
                                     value: ciudad.id,
@@ -120,28 +135,19 @@ const Editar = () => {
                                 onChange={(e) => {
                                     cambiarCiudad(e.label);
                                 }}
+                                value={userCity}
                                 placeholder="Ciudad"
                                 isSearchable
                                 noOptionsMessage={() => "Sin resultados"}
                                 classNames={{
-                                    control: () => "!text-sm !bg-gray-50 !border !border-gray-300 !text-colorFuente !sm:text-sm !rounded-lg !focus:ring-blue-500 !focus:border-blue-500 !w-full !p-0.5",
+                                    control: () => "!text-sm !bg-gray-50 !border !border-gray-300 !text-colorFuente !sm:text-sm !rounded-lg !focus:ring-blue-500 !focus:border-blue-500 !w-full !p-0.5 mt-1",
                                     input: (state) => state.isFocused ? "!ring-0 !shadow-none" : "",
                                     menuList: () => '!bg-gray-50'
                                 }}
                             />
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="telefono" className="block text-sm font-medium text-gray-700">Teléfono</label>
-                            <input
-                                type="text"
-                                id="telefono"
-                                name="telefono"
-                                value={userData.telefono}
-                                onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            />
-                        </div>
-                        <div className="flex justify-center">
+                        <div className='col-span-1'></div>
+                        <div className="flex justify-end col-span-1">
                             <button
                                 type="submit"
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
