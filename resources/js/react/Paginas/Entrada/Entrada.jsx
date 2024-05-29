919baf059ae1bb7eda61cacd981d77993fe83783
JesusPR04 <124920987+JesusPR04@users.noreply.github.com>
2024-05-29 14:42:14 +0200

    Mostrar eventos, falta el de la landing page

--- resources/js/react/Paginas/Entrada/Entrada.jsx
+++ resources/js/react/Paginas/Entrada/Entrada.jsx
@@ -1,10 +1,9 @@
 // src/components/Entradas.jsx
-import React, { useEffect, useState } from 'react';
-import { getEntradas } from '../../api/requests';
-import eventodefecto from '../../assets/eventodefecto.jpg';
+import React, { useEffect, useState } from "react";
+import { getEntradas } from "../../api/requests";
+import eventodefecto from "../../assets/eventodefecto.jpg";
 
-
-const BASE_URL = 'http://localhost:';
+const BASE_URL = "http://localhost:";
 
 const Entrada = () => {
     const [entradas, setEntradas] = useState([]);
@@ -13,18 +12,18 @@ const Entrada = () => {
 
     useEffect(() => {
         const fetchEntradas = async () => {
-            const token = localStorage.getItem('user-token'); // Asegúrate de tener el token almacenado en localStorage
+            const token = localStorage.getItem("user-token"); // Asegúrate de tener el token almacenado en localStorage
             if (token) {
                 try {
                     const data = await getEntradas(token);
                     setEntradas(data);
                 } catch (error) {
-                    setError('Error fetching entradas');
+                    setError("Error fetching entradas");
                 } finally {
                     setLoading(false);
                 }
             } else {
-                setError('Usuario no autenticado');
+                setError("Usuario no autenticado");
                 setLoading(false);
             }
         };
@@ -33,32 +32,107 @@ const Entrada = () => {
     }, []);
 
     if (loading) {
-        return <div className='min-h-[calc(100vh-436px)] text-center mt-10 text-colorFuente text-2xl font-bold'>Cargando...</div>;
+        return (
+            <div className="min-h-[calc(100vh-436px)] text-center mt-10 text-colorFuente text-2xl font-bold">
+                Cargando...
+            </div>
+        );
     }
 
     if (error) {
-        return <div className='min-h-[calc(100vh-436px)] text-center mt-10 text-red-500 text-2xl font-bold'>{error}</div>;
+        return (
+            <div className="min-h-[calc(100vh-436px)] text-center mt-10 text-red-500 text-2xl font-bold">
+                {error}
+            </div>
+        );
     }
 
     if (entradas.length === 0) {
-        return <div className='min-h-[calc(100vh-436px)] text-center mt-10 text-colorFuente text-2xl font-bold'>No tienes entradas</div>;
+        return (
+            <div className="min-h-[calc(100vh-436px)] text-center mt-10 text-colorFuente text-2xl font-bold">
+                No tienes entradas
+            </div>
+        );
     }
 
     return (
         <section className="bg-gray-100 py-12 md:py-16 lg:py-20 min-h-[calc(100vh-436px)]">
             <div className="container mx-auto px-4 md:px-6">
-                <h1 className="text-3xl md:text-4xl font-bold text-colorFuente mb-6 text-center">Entradas</h1>
+                <h1 className="text-3xl md:text-4xl font-bold text-colorFuente mb-6 text-center uppercase">
+                    Tus <span className="text-blue-500">Entradas</span>
+                </h1>
                 <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                     {entradas.map((entrada) => (
-                        <div key={entrada.id} className="border p-4 rounded shadow">
+                        <div
+                            key={entrada.id}
+                            className="border p-4 rounded shadow text-colorFuente bg-gray-100"
+                        >
                             <div className="mb-4">
-                                <img src={entrada.evento.ruta ? `${BASE_URL}${entrada.evento.ruta}` : eventodefecto} alt={entrada.evento.nombre} className="w-full h-48 object-cover rounded" />
+                                <img
+                                    src={
+                                        entrada.evento.ruta
+                                            ? `${BASE_URL}${entrada.evento.ruta}`
+                                            : eventodefecto
+                                    }
+                                    alt={entrada.evento.nombre}
+                                    className="w-full h-48 object-cover rounded"
+                                />
                             </div>
-                            <h2 className="text-xl font-bold mb-2">{entrada.evento.nombre}</h2>
-                            <p>Fecha: {entrada.evento.fecha}</p>
-                            <p>Hora: {entrada.evento.hora}</p>
-                            <p>Ubicación: {entrada.evento.localizacion}</p>
-                            <p>Cantidad: {entrada.cantidad}</p>
+                            <h2 className="text-xl font-bold mb-2 uppercase">
+                                {entrada.evento.nombre}
+                            </h2>
+                            <div className="grid grid-cols-2 xl:gap-x-4">
+                                <div className="col-span-2 xl:col-span-1 flex justify-between">
+                                    <span className="text-blue-500 font-bold">
+                                        Fecha
+                                    </span>
+                                    <span>{entrada.evento.fecha}</span>
+                                </div>
+                                <div className="col-span-2 xl:col-span-1 flex justify-between">
+                                    <span className="text-blue-500 font-bold">
+                                        Hora
+                                    </span>
+                                    <span>{entrada.evento.hora}</span>
+                                </div>
+                                <div className="col-span-2 xl:col-span-1 flex justify-between">
+                                    <span className="text-blue-500 font-bold">
+                                        Ubicación
+                                    </span>
+                                    <span>{entrada.evento.localizacion}</span>
+                                </div>
+                                <div className="col-span-2 xl:col-span-1 flex justify-between">
+                                    <span className="text-blue-500 font-bold">
+                                        Cantidad
+                                    </span>
+                                    <span>{entrada.cantidad}</span>
+                                </div>
+                            </div>
+                            <div className="flex justify-end">
+                                <button
+                                    className="bg-red-500 hover hover:bg-red-700 text-white 
+                                    font-bold py-2 px-4 rounded mt-4 flex gap-3"
+                                >
+                                    Cancelar
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        class="lucide lucide-trash-2"
+                                    >
+                                        <path d="M3 6h18" />
+                                        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                        <line x1="10" x2="10" y1="11" y2="17" />
+                                        <line x1="14" x2="14" y1="11" y2="17" />
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
                     ))}
                 </div>
--- resources/js/react/Paginas/Evento/Evento.jsx
+++ resources/js/react/Paginas/Evento/Evento.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getEventoById, fetchUserData, comprarEntrada } from '../../api/requests';
-import eventodefecto from '../../assets/eventodefecto.jpg';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+    getEventoById,
+    fetchUserData,
+    comprarEntrada,
+} from "../../api/requests";
+import eventodefecto from "../../assets/eventodefecto.jpg";
 
-const BASE_URL = 'http://localhost:';
+const BASE_URL = "http://localhost:";
 
 const Evento = () => {
     const { id } = useParams();
@@ -19,7 +23,7 @@ const Evento = () => {
                 const data = await getEventoById(id);
                 setEvento(data);
             } catch (error) {
-                setError('Error fetching evento');
+                setError("Error fetching evento");
             } finally {
                 setLoading(false);
             }
@@ -30,13 +34,13 @@ const Evento = () => {
 
     useEffect(() => {
         const fetchUser = async () => {
-            const token = localStorage.getItem('user-token'); // Asegúrate de tener el token almacenado en localStorage
+            const token = localStorage.getItem("user-token"); // Asegúrate de tener el token almacenado en localStorage
             if (token) {
                 try {
                     const userData = await fetchUserData(token);
                     setUser(userData);
                 } catch (error) {
-                    console.error('Error fetching user:', error.message);
+                    console.error("Error fetching user:", error.message);
                 }
             }
         };
@@ -45,18 +49,21 @@ const Evento = () => {
     }, []);
 
     const handleCompra = async () => {
-
         if (!user) {
-            alert('Debes estar logueado para comprar entradas');
+            alert("Debes estar logueado para comprar entradas");
             return;
         }
 
         try {
-            const data = await comprarEntrada({ idUsuario: user.id, idEvento: id, cantidad });
-            alert('Compra realizada con éxito');
+            const data = await comprarEntrada({
+                idUsuario: user.id,
+                idEvento: id,
+                cantidad,
+            });
+            alert("Compra realizada con éxito");
         } catch (error) {
-            console.error('Error comprando entrada:', error);
-            alert('Error comprando entrada');
+            console.error("Error comprando entrada:", error);
+            alert("Error comprando entrada");
         }
     };
 
@@ -73,58 +80,107 @@ const Evento = () => {
     }
 
     return (
-        <section className="bg-white dark:bg-gray-950 py-12 md:py-16 lg:py-20">
+        <section className="bg-gray-100 min-h-[calc(100vh-436px)] py-12 md:py-16 lg:py-20">
             <div className="container mx-auto px-4 md:px-6">
                 <div className="grid md:grid-cols-2 gap-8">
                     <div>
-                        <img src={evento.ruta ? `${BASE_URL}${evento.ruta}` : eventodefecto} alt={evento.nombre} className="w-full h-64 object-cover mb-4" />
+                        <img
+                            src={
+                                evento.ruta
+                                    ? `${BASE_URL}${evento.ruta}`
+                                    : eventodefecto
+                            }
+                            alt={evento.nombre}
+                            className="w-full h-full object-cover mb-4"
+                        />
                     </div>
-                    <div className="space-y-6">
+                    <div className="space-y-6 border p-4 rounded shadow bg-gray-100">
                         <div>
-                            <h1 className="text-3xl md:text-4xl font-bold text-gray-950 dark:text-white">
+                            <h1 className="text-3xl md:text-4xl font-bold text-colorFuente uppercase">
                                 {evento.nombre}
                             </h1>
-                            <p className="text-gray-500 dark:text-gray-400 mt-2">
+                            <p className="text-colorFuente mt-2">
                                 {/* Descripcion Corta */}
                             </p>
                         </div>
                         <div className="grid grid-cols-2 gap-4">
                             <div>
-                                <h3 className="text-lg font-medium text-gray-950 dark:text-white">Fecha</h3>
-                                <p className="text-gray-500 dark:text-gray-400">{evento.fecha}</p>
+                                <h3 className="text-lg font-medium text-colorFuente">
+                                    Fecha
+                                </h3>
+                                <p className="text-colorFuente">
+                                    {evento.fecha}
+                                </p>
                             </div>
                             <div>
-                                <h3 className="text-lg font-medium text-gray-950 dark:text-white">Hora</h3>
-                                <p className="text-gray-500 dark:text-gray-400">{evento.hora}</p>
+                                <h3 className="text-lg font-medium text-colorFuente">
+                                    Hora
+                                </h3>
+                                <p className="text-colorFuente">
+                                    {evento.hora}
+                                </p>
                             </div>
                             <div>
-                                <h3 className="text-lg font-medium text-gray-950 dark:text-white">Ubicación</h3>
-                                <p className="text-gray-500 dark:text-gray-400">{evento.localizacion}, {evento.ciudad.nombre}</p>
+                                <h3 className="text-lg font-medium text-colorFuente">
+                                    Ubicación
+                                </h3>
+                                <p className="text-colorFuente">
+                                    {evento.localizacion},{" "}
+                                    {evento.ciudad.nombre}
+                                </p>
                             </div>
                             <div>
-                                <h3 className="text-lg font-medium text-gray-950 dark:text-white">Aforo</h3>
-                                <p className="text-gray-500 dark:text-gray-400">{evento.aforoTotal}</p>
+                                <h3 className="text-lg font-medium text-colorFuente">
+                                    Aforo
+                                </h3>
+                                <p className="text-colorFuente">
+                                    {evento.aforoTotal}
+                                </p>
                             </div>
                         </div>
                         <div>
-                            <h3 className="text-lg font-medium text-gray-950 dark:text-white">Descripción</h3>
-                            <p className="text-gray-500 dark:text-gray-400">
+                            <h3 className="text-lg font-medium text-colorFuente">
+                                Descripción
+                            </h3>
+                            <p className="text-colorFuente">
                                 {evento.descripcion}
                             </p>
                         </div>
-                        <div className="flex gap-4">
+                        <div className="flex flex-col xl:flex-row gap-4">
                             <input
                                 type="number"
                                 value={cantidad}
                                 onChange={(e) => setCantidad(e.target.value)}
                                 min="1"
-                                className="border px-2 py-1"
+                                className="bg-gray-50 border border-gray-300 text-colorFuente sm:text-sm rounded-lg
+                                  focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                             />
-                            <button onClick={handleCompra} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <button
+                                onClick={handleCompra}
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            >
                                 Comprar Entradas
                             </button>
-                            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                                Añadir a Favoritos
+                            <button 
+                                className="bg-[#93C5FD] hover:bg-[#A5B4FC] text-colorFuente 
+                                font-bold py-2 px-4 rounded flex flex-row gap-3 justify-center xl:justify-start"
+                            >
+                                <span>Añadir a Favoritos</span>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    class="lucide lucide-star"
+                                    className="cursor-pointer"
+                                >
+                                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
                             </button>
                         </div>
                     </div>
