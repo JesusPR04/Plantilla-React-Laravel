752d9a4bd81d774b11482833dd1a05d306156d38
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-03 20:49:59 +0200

    header

--- resources/js/react/Components/Header.jsx
+++ resources/js/react/Components/Header.jsx
@@ -251,7 +251,7 @@ function Header() {
                                                                 width="16"
                                                                 height="16"
                                                                 fill="currentColor"
-                                                                class="bi bi-ticket-detailed-fill"
+                                                                className="bi bi-ticket-detailed-fill"
                                                                 viewBox="0 0 16 16">
                                                                 <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zm4 1a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5m0 5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5M4 8a1 1 0 0 0 1 1h6a1 1 0 1 0 0-2H5a1 1 0 0 0-1 1" />
                                                             </svg>
@@ -268,7 +268,7 @@ function Header() {
                                                                 width="18"
                                                                 height="18"
                                                                 fill="currentColor"
-                                                                class="bi bi-credit-card-2-front-fill"
+                                                                className="bi bi-credit-card-2-front-fill"
                                                                 viewBox="0 0 16 16">
                                                                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2.5 1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm0 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm3 0a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm3 0a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm3 0a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
                                                             </svg>
