4d8c7ad2fb3c8b0dfabcbfb8f83a4f4d3e31d51a
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-06 21:14:33 +0200

    quitar favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         @viteReactRefresh
         @vite('resources/js/app.js')
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ Vite::asset('../resources/js/react/assets/favicon.ico') }}" type="image/x-icon"> 
+        {{-- <link rel="shortcut icon" href="{{ Vite::asset('../resources/js/react/assets/favicon.ico') }}" type="image/x-icon">  --}}
     </head>
     <body>
        <div id='root'></div>
