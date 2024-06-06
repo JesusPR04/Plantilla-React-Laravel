1a073d73fec4cf8de3850a25a620d02c2b6d1b64
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-06 12:04:11 +0200

    login

--- resources/js/react/api/requests.js
+++ resources/js/react/api/requests.js
@@ -2,7 +2,7 @@ import { PATH_API } from './env.js'
 
 export const login = async (email, password) => {
   try {
-    const response = await fetch('http://localhost/api/login', {
+    const response = await fetch(`${PATH_API}/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
