f7cefa50440d430b414f191dfdf110001e19a050
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-06 21:43:18 +0200

    icono

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         @viteReactRefresh
         @vite('resources/js/app.js')
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ asset('../resources/js/react/assets/favicon.ico') }}" type="image/x-icon">  
+        <link rel="shortcut icon" href="{{ asset('Images/Event10_45014af6-9c0d-4cd3-b423-08a3be763672.jpg') }}" type="image/x-icon">  
     </head>
     <body>
        <div id='root'></div>
