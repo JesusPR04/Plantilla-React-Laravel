d804de99332257ab4c5221fe80ca2234c6a77e5a
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-06 12:39:45 +0200

    Solucionando el flowbite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,10 @@
         <title>Eventia</title>
         @viteReactRefresh
         @vite('resources/js/app.js')
-
         <!-- Favicon -->
         {{-- <link rel="shortcut icon" href="{{ Vite::asset('../resources/js/react/assets/favicon.ico') }}" type="image/x-icon"> --}}
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-
     </head>
     <body>
        <div id='root'></div>
-       <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     </body>
 </html>
