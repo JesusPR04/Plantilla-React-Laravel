e2ec8c4522ce157b0cb72288b8dfec6905d4cc3f
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-06 13:49:46 +0200

    :(

---
+++ app/Models/Imagenes.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Imagenes extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $table = 'imagenes';
+    
+    protected $fillable = [
+        'ruta',
+        'idEvento'
+    ];
+
+    protected $hidden = [
+        'updated_at',
+        'created_at'
+    ];
+
+    // Relación con Evento
+    public function evento()
+    {
+        return $this->belongsTo(Eventos::class, 'idEvento');
+    }
+}
