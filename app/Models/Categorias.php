10978ad06df87f7c01c52c51bbe3a1f9f15d989a
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-06 13:50:22 +0200

    :(

---
+++ app/Models/Categorias.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Categorias extends Model
+{
+    use HasFactory;
+
+    protected $table = "categorias";
+
+    protected $fillable = [
+        'nombre'
+    ];
+
+    protected $hidden = [
+        'updated_at',
+        'created_at'
+    ];
+    // Sacar todos los eventos que haya en la categoria
+    public function eventos(){
+        return $this->hasMany(Eventos::class);
+    }
+}
