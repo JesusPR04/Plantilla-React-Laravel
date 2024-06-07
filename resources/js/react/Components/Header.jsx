751fd3301863c54d268e74aa74360e1c0e1b5a3b
JesusPR04 <jesuspastorrobles12@gmail.com>
2024-06-07 15:52:05 +0200

    estrella del header favorito

--- resources/js/react/Components/Header.jsx
+++ resources/js/react/Components/Header.jsx
@@ -243,12 +243,12 @@ function Header() {
                                                                 width="18"
                                                                 height="18"
                                                                 viewBox="0 0 24 24"
-                                                                fill="currentColor"
-                                                                stroke="currentColor"
+                                                                fill="yellow"
+                                                                stroke="yellow" 
                                                                 strokeWidth="2"
                                                                 strokeLinecap="round"
                                                                 strokeLinejoin="round"
-                                                                className="lucide lucide-star text-yellow-500"
+                                                                className="lucide lucide-star"
                                                             >
                                                                 <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
                                                             </svg>
